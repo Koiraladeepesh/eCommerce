eadc8cf0536543d4a609fdb32bf519c395f51119
Koiraladeepesh <dipeshkoirala4@gmail.com>
2023-01-19 13:44:49 +0545

    building basket

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,10 @@ import SearchIcon from '@mui/icons-material/Search';
 import './Header.css'
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}, dispatch] = useStateValue();
   return (
     <div className='header'>
         <Link to='/'>
@@ -34,7 +36,7 @@ function Header() {
             <Link to='/checkout'>
                     <div className="header-menu-basket">
                         <ShoppingBasketIcon/>
-                    <span className="header-menu-two header-menu-basket-count">0</span>
+                    <span className="header-menu-two header-menu-basket-count">{basket?.length}</span>
                 </div>
             </Link>
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
 function Product({id, title, image, price, rating}) {
+
+  const [{basket}, dispatch] = useStateValue();
+
+  console.log("this is the basket>>>", basket);
+
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: 'ADD-TO-BASKET',
+      item:{
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    })
+  }
   return (
     <div className='product'>
         <div className="product-info">
@@ -18,7 +37,7 @@ function Product({id, title, image, price, rating}) {
         </div>
         <img src={image} alt="" />
 
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
